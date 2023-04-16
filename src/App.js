8a6b6fa1d85e575b65c12bcbc7845cb0e4c1d85d
Arief Badal <1042170@hr.nl>
2023-04-16 15:24:24 +0200

    Add first version of quote generator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react"
+import axios from 'axios'
 
 function App() {
+  const [quote, setQuote] = useState([]);
+
+  const config = {
+    headers:{
+      'X-Api-Key': '9ljd8SJvONzkK0vgwPVvl6ur6bvRzs9eL4IYSwKp'
+    }
+  }
+  const url = 'https://api.api-ninjas.com/v1/quotes?category=success'
+  const fetchQuote = () => {
+    axios.get(url, config)
+    .then(res => {
+      setQuote(res.data)
+      console.log(res.data)
+    })
+    .catch(err=> console.log(err))
+
+  }
+  
+  // event listener
+  function ShowQuote(){
+    fetchQuote()
+  }
+  // eslint-disable-next-line
+  useEffect(()=> ShowQuote(), [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='quote-card'>
+      <h1>Quote generator</h1>
+      {quote.map((item) => (
+        <div className='quote-card-content'>
+          <p>{item.quote}</p>
+          <p>Author: {item.author}</p>
+        </div>
+
+      ))}
+      <button onClick={ShowQuote}>Get new Quote</button>
     </div>
   );
 }
